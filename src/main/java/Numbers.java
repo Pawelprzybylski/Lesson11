4dd99ed6ac3dcfc0c0d32820654083f546463ebf
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-14 10:30:35 +0200

    Commit

---
+++ src/main/java/Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers {
+
+    public static boolean divide(int number) {
+        return number % 2 == 0;
+    }
+
+    public static int add(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+}
---
+++ src/test/java/NumbersTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NumbersTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {20, 8, 2, 4})
+    void shouldVerifyNumberIsDivideBy2(int input) {
+        Assertions.assertTrue(Numbers.divide(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"First number: 20", "Second number: 8", "Third number: 2", "Fourth Number: 4"}, delimiter = ':')
+    void shouldVerifyStringTrimNumbers(String input, String expected) {
+        String actualValue = expected;
+        Assertions.assertEquals(expected, actualValue);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {25, 21, 31, 5})
+    void shouldVerifyNumberIsNotDivideBy2(int input) {
+        Assertions.assertFalse(Numbers.divide(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {285, 276, 186, 456})
+    void shouldVerifyAddedNumberIsEqual15(int input) {
+        Assertions.assertEquals(15, Numbers.add(input));
+    }
+
+}
+
