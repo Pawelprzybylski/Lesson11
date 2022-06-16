c1a69b7f12e45ffc0bc0000bbff8252cd88e9791
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-16 19:31:23 +0200

    Commit

---
+++ src/main/java/Months.java
@@ -0,0 +1,24 @@
+import java.time.Month;
+
+public class Months {
+    static Month september = Month.of(Seasons.Autumn.getMonth1());
+
+    public static void getMonth() {
+        System.out.println(september);
+    }
+
+    public static void main(String[] args) {
+        getMonth();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
