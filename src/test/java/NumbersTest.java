843642ba41c0e91f6c857e3ec89d1460a2140541
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-06 09:26:57 +0200

    Commit

--- src/test/java/NumbersTest.java
+++ src/test/java/NumbersTest.java
@@ -7,27 +7,42 @@ public class NumbersTest {
 
     @ParameterizedTest
     @ValueSource(ints = {20, 8, 2, 4})
-    void shouldVerifyNumberIsDivideBy2(int input) {
+    void shouldVerifyNumberIsEven(int input) {
         Assertions.assertTrue(Numbers.isEvenNumber(input));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"First number: 20", "Second number: 8", "Third number: 2", "Fourth Number: 4"}, delimiter = ':')
-    void shouldVerifyStringTrimNumbers(String input, String expected) {
-        String actualValue = expected;
-        Assertions.assertEquals(expected, actualValue);
+    @CsvSource(value = {"20: true", "6: true", "8: true", "10: true"}, delimiter = ':')
+    void shouldVerifyNumberIsEven(int input, boolean expected) {
+
+        Assertions.assertEquals(Numbers.isEvenNumber(input), expected);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {25, 21, 31, 5})
-    void shouldVerifyNumberIsNotDivideBy2(int input) {
+    void shouldVerifyNumberIsNotEven(int input) {
         Assertions.assertFalse(Numbers.isEvenNumber(input));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"19: false", "11: false", "5: false", "3: false"}, delimiter = ':')
+    void shouldVerifyNumberIsNotEven(int input, boolean expected) {
+
+        Assertions.assertEquals(Numbers.isEvenNumber(input), expected);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {285, 276, 186, 456})
-    void shouldVerifyAddedNumberIsEqual15(int input) {
+    void shouldAddDigitsFromNumber(int input) {
         Assertions.assertEquals(15, Numbers.add(input));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value= {"23:5", "21:3", "11:2", "85:13"}, delimiter = ':')
+    void  shouldAddDigitsFromNumber(int input, int expected){
+        Assertions.assertEquals(Numbers.add(input),expected);
     }
 
 }
